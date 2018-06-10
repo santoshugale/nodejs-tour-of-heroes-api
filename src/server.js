99aad8129687af92759bf0a5b27af0b1235cdcad
Santosh Ugale <santosh_ugale@nuance.com>
2018-06-10 11:47:45 +0530

    enabled CORS Request

--- src/server.js
+++ src/server.js
@@ -8,12 +8,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-//     next();
-// });
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
+
 routes(app);
 app.listen(port, () => {
     console.log('Started server on port  ' + port);
